3df3b0c44500a70856607c6f53de2a2f925cbaee
politipet <politipet@github.users>
2023-07-09 22:31:29 +0200

    rename page.url => page_url, fix rounding bug

--- fetch.mk
+++ fetch.mk
@@ -10,9 +10,9 @@ stats:
 	@wc -l all-data.txt
 
 
-page.url = $(VOTE)?order=recent&per_page=100&
+page_url = $(VOTE)?order=recent&per_page=100&
 page.%:
-	curl -s -H "Accept: text/html" "$(page.url)page=$*" \
+	curl -s -H "Accept: text/html" "$(page_url)page=$*" \
 	| egrep 'progress__bar__number|card__button|area_id%' \
 	| sed '/progress__bar/ { s: ::g; s:.*__number">:= :; s:<.*:: }' \
 	| sed '/area_id/ { s:.*=:c :; s:">.*:: }' \
@@ -26,6 +26,6 @@ page.%:
 nb_pages = $(shell curl -s -H "Accept: text/html" $(VOTE) \
 	| grep -A1 initiatives-count \
 	| tail -1 \
-	| awk '{printf("%.0f", $$1/100)}' \
+	| awk '{print $$1/100 + ($$1 % 100 ? 1 : 0) }' \
 )
 all_pages = $(shell seq $(nb_pages))
