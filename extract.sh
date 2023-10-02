74b707b40936a5d4ec1033ce6f6318fd69614f75
politipet <politipet@github.users>
2023-10-02 22:53:18 +0200

    add extract.sh

---
+++ extract.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+usage() {
+	echo "usage: $0 <pet num>"
+	exit $1
+}
+
+shopt -s extglob; num="+([0-9])"
+
+case $1 in
+	$num)
+		TARGET=i-$1 ;;
+	-h|--help)
+		usage 0 ;;
+	*)
+		usage 1 ;;
+esac
+
+data=all-data.txt
+
+git log --oneline $data | cut -f 1 -d ' ' | tac |\
+while read n; do
+	git show $n $data | egrep "^[+]$TARGET" | {
+	read val && {
+		git log -1 $n --format=%ad --date=format:'%F %T'
+		echo $val
+	} | xargs; }
+done | awk '{print $1, $2 "\t" $5}'
