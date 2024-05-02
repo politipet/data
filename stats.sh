80df436df9ab8a000aebb62d00f60739fb23f617
politipet <politipet@github.users>
2024-05-02 18:31:52 +0200

    fix stats.sh

--- stats.sh
+++ stats.sh
@@ -20,14 +20,15 @@ sum_votes() {
 
 sum_days() {
 	awk '
-		{ n += $3 }
 		!day	{ day = $1 }
 		END	{ print day, n }
 		$1 != day {
 			print day, n
 			fflush()
 			day = $1; n = $3
+			next
 		}
+		{ n += $3 }
 	'
 }
 
@@ -41,23 +42,36 @@ bars() {
 }
 
 get_commit_votes() {
-	git show --word-diff $1 $data		\
-	| fix_word_diff				\
-	| grep '^i-.*\[-'			\
-	| sed 's:[-+]: :g'			\
-	| awk '	{ n += ($8 - $6) }
-		END { print n }'
+	git show $1 $data			\
+	| get_score_diffs_from_diff		\
+	| discard_closed_or_readded		\
+	| awk '{ t += $3 } END { print t }'
+}
+
+discard_closed_or_readded() {
+	awk '	$5 == "-"		{next} # closed
+		$5 == "+" && $3 > 3000	{next} # re-added
+	{print}
+	'
+}
+
+get_score_diffs_from_diff() {
+	egrep '[+-]i-' | sed 's/i-/ i-/'	\
+	| awk '{
+		id = $2; comm = $3; score = $4
+		s[id] = score
+		c[id] = comm
+		d[id] += int($1 score)
+		x[id] = (x[id] $1) == "-+" ? "" : $1
+	}
+	END { for (id in s) print id, c[id], d[id], s[id], x[id] }
+	'
 }
 
 get_commit_date() {
 	TZ=$TZ git show -s $1			\
 		--format=%ad 			\
 		--date=format-local:'%F %T'
-}
-
-fix_word_diff() {
-	sed 's:\]i:]\ni:g' |\
-	tr '\n' : | sed 's,\]:\[[^{]*,],g' | tr : '\n'
 }
 
 #sum_votes | sed 's/ /\t/2'
